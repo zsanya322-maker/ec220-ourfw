48e3c27846ea7f3fd67fc512cb82e3974e89224b
AlexB322 <264132173+zsanya322-maker@users.noreply.github.com>
2026-08-17 13:13:01 +0500

    fix(v0.7b): validate obfs mode in plain Hysteria build

--- research/hysteria/ec220_tproxy/factory_plain.go
+++ research/hysteria/ec220_tproxy/factory_plain.go
@@ -5,21 +5,30 @@ package main
 import (
 	"errors"
 	"net"
+	"strings"
 
 	hyclient "github.com/apernet/hysteria/core/v2/client"
 )
 
 type packetConnFactory struct {
+	obfsType string
 	password string
 }
 
-func newPacketConnFactory(password string) hyclient.ConnFactory {
-	return &packetConnFactory{password: password}
+func newPacketConnFactory(obfsType, password string) hyclient.ConnFactory {
+	return &packetConnFactory{obfsType: strings.ToLower(strings.TrimSpace(obfsType)), password: password}
 }
 
 func (f *packetConnFactory) New(_ net.Addr) (net.PacketConn, error) {
-	if f.password != "" {
+	switch f.obfsType {
+	case "", "plain":
+		if f.password != "" {
+			return nil, errors.New("obfs password supplied for plain transport")
+		}
+		return net.ListenUDP("udp", nil)
+	case "salamander":
 		return nil, errors.New("this binary was built without Salamander support")
+	default:
+		return nil, errors.New("unsupported obfs type")
 	}
-	return net.ListenUDP("udp", nil)
 }
