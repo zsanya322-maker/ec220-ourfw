ef197f52bb899ab4506e5397efc043dceac1b4c7
AlexB322 <264132173+zsanya322-maker@users.noreply.github.com>
2026-08-17 13:13:11 +0500

    fix(v0.7b): validate obfs mode in Salamander Hysteria build

--- research/hysteria/ec220_tproxy/factory_salamander.go
+++ research/hysteria/ec220_tproxy/factory_salamander.go
@@ -3,18 +3,21 @@
 package main
 
 import (
+	"errors"
 	"net"
+	"strings"
 
 	hyclient "github.com/apernet/hysteria/core/v2/client"
 	"github.com/apernet/hysteria/extras/v2/obfs"
 )
 
 type packetConnFactory struct {
+	obfsType string
 	password string
 }
 
-func newPacketConnFactory(password string) hyclient.ConnFactory {
-	return &packetConnFactory{password: password}
+func newPacketConnFactory(obfsType, password string) hyclient.ConnFactory {
+	return &packetConnFactory{obfsType: strings.ToLower(strings.TrimSpace(obfsType)), password: password}
 }
 
 func (f *packetConnFactory) New(_ net.Addr) (net.PacketConn, error) {
@@ -22,13 +25,26 @@ func (f *packetConnFactory) New(_ net.Addr) (net.PacketConn, error) {
 	if err != nil {
 		return nil, err
 	}
-	if f.password == "" {
+	switch f.obfsType {
+	case "", "plain":
+		if f.password != "" {
+			_ = conn.Close()
+			return nil, errors.New("obfs password supplied for plain transport")
+		}
 		return conn, nil
-	}
-	wrapped, err := obfs.WrapPacketConnSalamander(conn, []byte(f.password))
-	if err != nil {
+	case "salamander":
+		if f.password == "" {
+			_ = conn.Close()
+			return nil, errors.New("Salamander requires a password")
+		}
+		wrapped, err := obfs.WrapPacketConnSalamander(conn, []byte(f.password))
+		if err != nil {
+			_ = conn.Close()
+			return nil, err
+		}
+		return wrapped, nil
+	default:
 		_ = conn.Close()
-		return nil, err
+		return nil, errors.New("unsupported obfs type")
 	}
-	return wrapped, nil
 }
